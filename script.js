7b8e4c45067470865f21c190950c09da2f4eeff1
Wan338 <78829745@qq.com>
2026-03-03 20:38:14 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+function functionClick(){
+    var  wrapperValue = document.getElementById("wrapper");
+    wrapperValue.style.display = "block";
+}
+
+function functionClose(){
+    var  wrapperValue = document.getElementById("wrapper");
+    wrapperValue.style.display = "none";
+}
